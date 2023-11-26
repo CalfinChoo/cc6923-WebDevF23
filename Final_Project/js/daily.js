ff0e72ef553f4d7ff31f2043c0bd63c6ad685f51
Calvin Chu <cchu00@stuy.edu>
2023-11-26 18:23:28 -0500

    finish daily

--- Final_Project/js/daily.js
+++ Final_Project/js/daily.js
@@ -13,25 +13,45 @@ const seed =
 Math.seedrandom(seed);
 
 var squares, modal, cat_squares, lives, life_count, list, search;
+var seconds, timerInterval;
 var activeSquares;
+
+var used = new Set([]);
+var currRow, currCol;
 var numLives = 1;
+var correct = 0;
 
-const generateUniqueRandomKeys = (object, numberOfKeys) => {
-  const uniqueKeys = new Set();
-  const keys = Object.keys(object);
+const generateUniqueRandomKeys = (object) => {
+  var out = [];
+  var keys = Object.keys(object);
 
-  while (uniqueKeys.size < numberOfKeys) {
+  for (let i = 0; i < COLS; ++i) {
     const randomIndex = Math.floor(Math.random() * keys.length);
     const randomKey = keys[randomIndex];
 
-    if (!uniqueKeys.has(randomKey)) {
-      uniqueKeys.add(randomKey);
+    out.push(randomKey);
+    keys.splice(i, 1);
+  }
+  for (let key of out) {
+    if (!excludeCombos.hasOwnProperty(key)) continue;
+    for (let type of excludeCombos[key]) {
+      const index = keys.indexOf(type);
+      if (index !== -1) {
+        keys.splice(index, 1);
+      }
     }
   }
+  for (let i = 0; i < ROWS; ++i) {
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomKey = keys[randomIndex];
 
-  return Array.from(uniqueKeys);
+    out.push(randomKey);
+    keys.splice(i, 1);
+  }
+
+  return out;
 };
-const generatedKeys = generateUniqueRandomKeys(categories, ROWS + COLS);
+const generatedKeys = generateUniqueRandomKeys(categories);
 
 const formatName = (name) => {
   function replaceDashes(inputString) {
@@ -74,7 +94,7 @@ const sortArr = (arr, targetSet) => {
 
     const index = str.indexOf(targetSet);
     if (index !== -1) {
-      exactMatchScore++;
+      ++exactMatchScore;
       matchingOrderScore += index;
     }
 
@@ -101,32 +121,133 @@ const sortArr = (arr, targetSet) => {
   return arr.sort(sortByMatchScore);
 };
 
-const onStay = (e) => {
-  let r = parseInt(e.target.id[ROW_INDEX]);
-  let c = parseInt(e.target.id[COL_INDEX]);
-  if (activeSquares[r][c] == 0) e.target.classList.add("selected");
+const updateGameState = () => {
+  if (numLives === 0) {
+    clearInterval(timerInterval);
+    $("#summaryModal").on("show.bs.modal", function () {
+      var modal = $(this);
+      modal.find(".modal-title").text("Puzzle Summary");
+      modal
+        .find(".modal-body")
+        .prepend('<img class="summaryImg" src="./img/loser.jpg"/>')
+        .prepend('<h5 class="text-center">You have run out of guesses!</h5>')
+        .prepend('<h4 class="text-center">You Lost!</h4>');
+    });
+    $("#summaryModal").modal("toggle");
+  } else if (correct === ROWS * COLS) {
+    clearInterval(timerInterval);
+    $("#summaryModal").on("show.bs.modal", function () {
+      var modal = $(this);
+      modal.find(".modal-title").text("Puzzle Summary");
+      modal
+        .find(".modal-body")
+        .prepend('<img class="summaryImg" src="./img/winner.png"/>')
+        .prepend(
+          '<h5 class="text-center">You have solved the puzzle in ' +
+            formatTime(seconds) +
+            "!</h5>"
+        )
+        .prepend('<h4 class="text-center">You Won!</h4>');
+    });
+    $("#summaryModal").modal("toggle");
+  }
 };
-const onExit = (e) => {
-  let r = parseInt(e.target.id[ROW_INDEX]);
-  let c = parseInt(e.target.id[COL_INDEX]);
-  if (activeSquares[r][c] == 0) e.target.classList.remove("selected");
+
+const formatTime = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedTime =
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(remainingSeconds).padStart(2, "0");
+
+  return formattedTime;
 };
-const openModal = (e) => {
-  let r = parseInt(e.target.id[ROW_INDEX]);
-  let c = parseInt(e.target.id[COL_INDEX]);
+
+const updateTimer = () => {
+  seconds++;
+  document.getElementById("timer").innerText = formatTime(seconds);
+};
+
+const onStay = (square) => {
+  let r = parseInt(square.id[ROW_INDEX]);
+  let c = parseInt(square.id[COL_INDEX]);
+  if (activeSquares[r][c] == 0 && numLives !== 0)
+    square.classList.add("selected");
+};
+const onExit = (square) => {
+  let r = parseInt(square.id[ROW_INDEX]);
+  let c = parseInt(square.id[COL_INDEX]);
+  if (activeSquares[r][c] == 0 && numLives !== 0)
+    square.classList.remove("selected");
+};
+const openModal = (square) => {
+  let r = parseInt(square.id[ROW_INDEX]);
+  let c = parseInt(square.id[COL_INDEX]);
+  if (activeSquares[r][c] !== 0 || numLives === 0) return;
+  currRow = r;
+  currCol = c;
   $("#selectionModal").on("show.bs.modal", function () {
     list.innerHTML = "";
-    var recipient = e.target.id;
 
     var modal = $(this);
-
+    modal.find(".modal-title").text("Make Your Guess");
     modal
       .find(".modal-cat")
-      .text(generatedKeys[COLS + r] + "/" + generatedKeys[c]);
+      .text(generatedKeys[COLS + currRow] + "/" + generatedKeys[currCol]);
     modal.find(".modal-body input").attr("placeholder", "Search Pokémon...");
     modal.find(".modal-body input").val("");
   });
-  if (activeSquares[r][c] == 0) $("#selectionModal").modal("toggle");
+  $("#selectionModal").modal("toggle");
+};
+const handleSelect = (i) => {
+  $("#selectionModal").modal("toggle");
+  let cats = [generatedKeys[COLS + currRow], generatedKeys[currCol]];
+  var incorrect;
+  for (let cat of cats) {
+    incorrect = true;
+    switch (categories[cat].type) {
+      case "mega":
+        if (/-mega(-x|-y)?$/.test(monData[i].name)) incorrect = false;
+        break;
+      case "gmax":
+        if (monData[i].name.endsWith("-gmax")) incorrect = false;
+        break;
+      case "type":
+        for (let type of monData[i].types) {
+          if (type.type.name.toLowerCase() === cat.toLowerCase())
+            incorrect = false;
+        }
+        break;
+    }
+    if (incorrect) {
+      console.log("Incorrect");
+      numLives = numLives - 1 < 0 ? 0 : numLives - 1;
+      life_count.innerHTML = numLives;
+      break;
+    }
+  }
+  if (!incorrect) {
+    var target = document.getElementById("b" + currRow + currCol);
+    activeSquares[currRow][currCol] = 1;
+    ++correct;
+    used.add(monData[i].name);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = monData[i].frontImage;
+    imgElement.alt = monData[i].name;
+    const spanElement = document.createElement("span");
+    spanElement.innerHTML =
+      "<strong>" + formatName(monData[i].name) + "</strong>";
+    spanElement.classList.add(...nameBanner);
+    target.appendChild(imgElement);
+    target.appendChild(spanElement);
+  }
+  updateGameState();
 };
 const updateList = (e) => {
   list.innerHTML = "";
@@ -134,7 +255,10 @@ const updateList = (e) => {
   for (let i = 0; i < monData.length; ++i) {
     if (
       excludeSet.has(monData[i].name) ||
-      !containsSubsequence(monData[i].name, e.target.value)
+      !containsSubsequence(
+        monData[i].name.toLowerCase(),
+        e.target.value.toLowerCase()
+      )
     )
       continue;
     arr.push(i);
@@ -163,6 +287,10 @@ const updateList = (e) => {
     buttonElement.classList.add(...liButtonClassList);
     buttonElement.type = "button";
     buttonElement.innerHTML = "<strong>Select</strong>";
+    buttonElement.disabled = used.has(monData[i].name);
+    buttonElement.addEventListener("click", () => {
+      handleSelect(i);
+    });
 
     liElement.appendChild(divElement);
     liElement.appendChild(buttonElement);
@@ -184,9 +312,15 @@ document.addEventListener("DOMContentLoaded", () => {
     .map(() => Array(COLS).fill(0));
 
   for (let i = 0; i < squares.length; ++i) {
-    squares[i].addEventListener("mouseover", onStay);
-    squares[i].addEventListener("mouseout", onExit);
-    squares[i].addEventListener("click", openModal);
+    squares[i].addEventListener("mouseover", () => {
+      onStay(squares[i]);
+    });
+    squares[i].addEventListener("mouseout", () => {
+      onExit(squares[i]);
+    });
+    squares[i].addEventListener("click", () => {
+      openModal(squares[i]);
+    });
   }
 
   for (let i = 0; i < cat_squares.length; ++i) {
@@ -212,5 +346,7 @@ document.addEventListener("DOMContentLoaded", () => {
   life_count.innerHTML = numLives;
 
   search.addEventListener("input", updateList);
-  //console.log(monData);
+
+  seconds = 0;
+  timerInterval = setInterval(updateTimer, 1000);
 });
--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -2,86 +2,147 @@ const categories = {
   Normal: {
     img: "./img/types/normal.png",
     alt: "Normal",
+    type: "type",
   },
   Fighting: {
     img: "./img/types/fighting.png",
     alt: "Fighting",
+    type: "type",
   },
   Flying: {
     img: "./img/types/flying.png",
     alt: "Flying",
+    type: "type",
   },
   Poison: {
     img: "./img/types/poison.png",
     alt: "Poison",
+    type: "type",
   },
   Ground: {
     img: "./img/types/ground.png",
     alt: "Ground",
+    type: "type",
   },
   Rock: {
     img: "./img/types/rock.png",
     alt: "Rock",
+    type: "type",
   },
   Bug: {
     img: "./img/types/bug.png",
     alt: "Bug",
+    type: "type",
   },
   Ghost: {
     img: "./img/types/ghost.png",
     alt: "Ghost",
+    type: "type",
   },
   Steel: {
     img: "./img/types/steel.png",
     alt: "Steel",
+    type: "type",
   },
   Fire: {
     img: "./img/types/fire.png",
     alt: "Fire",
+    type: "type",
   },
   Water: {
     img: "./img/types/water.png",
     alt: "Water",
+    type: "type",
   },
   Grass: {
     img: "./img/types/grass.png",
     alt: "grass",
+    type: "type",
   },
   Electric: {
     img: "./img/types/electric.png",
     alt: "Electric",
+    type: "type",
   },
   Psychic: {
     img: "./img/types/psychic.png",
     alt: "Psychic",
+    type: "type",
   },
   Ice: {
     img: "./img/types/ice.png",
     alt: "Ice",
+    type: "type",
   },
   Dragon: {
     img: "./img/types/dragon.png",
     alt: "Dragon",
+    type: "type",
   },
   Dark: {
     img: "./img/types/dark.png",
     alt: "Dark",
+    type: "type",
   },
   Fairy: {
     img: "./img/types/fairy.png",
     alt: "Fairy",
+    type: "type",
   },
-  //   Region: {
-  //     img: null,
-  //     alt: "Region"
-  //   },
+  // Kanto: {
+  //   img: null,
+  //   alt: "Kanto",
+  //   type: "region",
+  // },
+  // Johto: {
+  //   img: null,
+  //   alt: "Johto",
+  //   type: "region",
+  // },
+  // Hoenn: {
+  //   img: null,
+  //   alt: "Hoenn",
+  //   type: "region",
+  // },
+  // Sinnoh: {
+  //   img: null,
+  //   alt: "Sinnoh",
+  //   type: "region",
+  // },
+  // Unova: {
+  //   img: null,
+  //   alt: "Unova",
+  //   type: "region",
+  // },
+  // Kalos: {
+  //   img: null,
+  //   alt: "Kalos",
+  //   type: "region",
+  // },
+  // Alola: {
+  //   img: null,
+  //   alt: "Alola",
+  //   type: "region",
+  // },
+  // Galar: {
+  //   img: null,
+  //   alt: "Galar",
+  //   type: "region",
+  // },
+  // Paldea: {
+  //   img: null,
+  //   alt: "Paldea",
+  //   type: "region",
+  // },
   Mega: {
     img: null,
     alt: "Mega Evolution",
+    type: "mega",
   },
   GMAX: {
     img: null,
     alt: "GMAX Form",
+    type: "gmax",
   },
 };
 
@@ -94,6 +155,7 @@ const liClassList = [
 ];
 const liSpanClassList = [".d-inline-flex", "align-self-center", "px-2"];
 const liButtonClassList = ["sub-btn", "btn", "btn-danger"];
+const nameBanner = ["banner", "position-absolute", "bottom-0"];
 
 const excludeSet = new Set([
   "pikachu-rock-star",
@@ -135,3 +197,19 @@ const excludeSet = new Set([
   "miraidon-aquatic-mode",
   "miraidon-glide-mode",
 ]);
+
+const excludeCombos = {
+  Normal: ["Ice", "Bug", "Steel", "Rock"],
+  Fire: ["Fairy"],
+  Ice: ["Poison", "Normal"],
+  Ground: ["Fairy"],
+  Bug: ["Dragon", "Normal"],
+  Rock: ["Ghost", "Normal"],
+  Steel: ["Normal"],
+  Fairy: ["Fire", "Ground"],
+  Poison: ["Ice"],
+  Dragon: ["Bug"],
+  Ghost: ["Rock"],
+  GMAX: ["Mega"],
+  Mega: ["GMAX"],
+};
